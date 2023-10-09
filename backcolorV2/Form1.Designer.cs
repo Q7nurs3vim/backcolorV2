d202faa94a466591cb7adfc5fd145361b284b939
Luz <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-09 13:27:09 +0300

    Add files via upload

---
+++ backcolorV2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace backcolorV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPurple = new System.Windows.Forms.CheckBox();
+            this.cbBlack = new System.Windows.Forms.CheckBox();
+            this.cbGreen = new System.Windows.Forms.CheckBox();
+            this.cbYellow = new System.Windows.Forms.CheckBox();
+            this.cbBlue = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cbPurple
+            // 
+            this.cbPurple.AutoSize = true;
+            this.cbPurple.Location = new System.Drawing.Point(24, 37);
+            this.cbPurple.Name = "cbPurple";
+            this.cbPurple.Size = new System.Drawing.Size(44, 17);
+            this.cbPurple.TabIndex = 0;
+            this.cbPurple.Text = "Mor";
+            this.cbPurple.UseVisualStyleBackColor = true;
+            this.cbPurple.CheckedChanged += new System.EventHandler(this.cbMor_CheckedChanged);
+            // 
+            // cbBlack
+            // 
+            this.cbBlack.AutoSize = true;
+            this.cbBlack.Location = new System.Drawing.Point(24, 200);
+            this.cbBlack.Name = "cbBlack";
+            this.cbBlack.Size = new System.Drawing.Size(52, 17);
+            this.cbBlack.TabIndex = 1;
+            this.cbBlack.Text = "Siyah";
+            this.cbBlack.UseVisualStyleBackColor = true;
+            this.cbBlack.CheckedChanged += new System.EventHandler(this.cbsiyah_CheckedChanged);
+            // 
+            // cbGreen
+            // 
+            this.cbGreen.AutoSize = true;
+            this.cbGreen.Location = new System.Drawing.Point(24, 162);
+            this.cbGreen.Name = "cbGreen";
+            this.cbGreen.Size = new System.Drawing.Size(48, 17);
+            this.cbGreen.TabIndex = 2;
+            this.cbGreen.Text = "Yeşil";
+            this.cbGreen.UseVisualStyleBackColor = true;
+            this.cbGreen.CheckedChanged += new System.EventHandler(this.cbYesil_CheckedChanged);
+            // 
+            // cbYellow
+            // 
+            this.cbYellow.AutoSize = true;
+            this.cbYellow.Location = new System.Drawing.Point(24, 123);
+            this.cbYellow.Name = "cbYellow";
+            this.cbYellow.Size = new System.Drawing.Size(44, 17);
+            this.cbYellow.TabIndex = 3;
+            this.cbYellow.Text = "Sarı";
+            this.cbYellow.UseVisualStyleBackColor = true;
+            this.cbYellow.CheckedChanged += new System.EventHandler(this.cbYellow_CheckedChanged);
+            // 
+            // cbBlue
+            // 
+            this.cbBlue.AutoSize = true;
+            this.cbBlue.Location = new System.Drawing.Point(24, 79);
+            this.cbBlue.Name = "cbBlue";
+            this.cbBlue.Size = new System.Drawing.Size(49, 17);
+            this.cbBlue.TabIndex = 4;
+            this.cbBlue.Text = "Mavi";
+            this.cbBlue.UseVisualStyleBackColor = true;
+            this.cbBlue.CheckedChanged += new System.EventHandler(this.cbMavi_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 242);
+            this.Controls.Add(this.cbBlue);
+            this.Controls.Add(this.cbYellow);
+            this.Controls.Add(this.cbGreen);
+            this.Controls.Add(this.cbBlack);
+            this.Controls.Add(this.cbPurple);
+            this.Name = "Form1";
+            this.Text = "Parti Parti Yaaaee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbPurple;
+        private System.Windows.Forms.CheckBox cbBlack;
+        private System.Windows.Forms.CheckBox cbGreen;
+        private System.Windows.Forms.CheckBox cbYellow;
+        private System.Windows.Forms.CheckBox cbBlue;
+    }
+}
+
---
+++ backcolorV2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace backcolorV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbMor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbPurple.Checked == true)
+            {
+                this.BackColor = Color.Purple;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+            }
+        }
+
+        private void cbMavi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbBlue.Checked == true)
+            {
+                this.BackColor = Color.Blue;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+            }
+        }
+
+        private void cbYellow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbYellow.Checked == true)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+            }
+        }
+
+        private void cbYesil_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbGreen.Checked == true)
+            {
+                this.BackColor = Color.Green;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+            }
+        }
+
+        private void cbsiyah_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbBlack.Checked == true)
+            {
+                this.BackColor = Color.Black;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+            }
+        }
+    }
+    
+}
